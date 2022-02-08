0a148272c01d25c7b74dfc938ea29a13f5a1afb8
yoloyoung <94785366+yoloyoung@users.noreply.github.com>
2022-02-08 12:50:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,21 @@ import json
 from all_array import *
 import threading
 import time
-async def get_main():
+from bs4 import BeautifulSoup
+import requests
+
+async def getMain():
     for i in range(len(array)):
         try:
-            response = f"wss://stream.binance.com:9443/stream?streams={array[i]}usdt@miniTicker"
+            response = f'wss://stream.binance.com:9443/stream?streams={array[i]}usdt@miniTicker'
             async with websockets.connect(response) as client:
-                data = json.loads(await client.recv())["data"]                                              
-                response_time = time.localtime(data['E'] // 1000)
-                print('Назва монети ->', data['s'],'Час ->',f'{response_time.tm_hour}:{response_time.tm_min}:{response_time.tm_sec}','Ціна за монету ->', data['c'],'Найнижча ціна за монету за 24 часа ->', data['l'],'Найвища ціна за монету за 24 часа ->', data['h'],'Обьем на площадкі ->', data['v'])
+                data = json.loads(await client.recv())['data']                                              
+                responseTime = time.localtime(data['E'] // 1000)
+                print('Назва монети ->', data['s'],'Час ->',f'{responseTime.tm_hour}:{responseTime.tm_min}:{responseTime.tm_sec}','Ціна за монету ->', data['c'],'Найнижча ціна за монету за 24 часа ->', data['l'],'Найвища ціна за монету за 24 часа ->', data['h'],'Обьем на площадкі ->', data['v'])
         except:
             print("Ты еблан, ты забанен")
 
-
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(get_main())
+    loop.run_until_complete(getMain())
+    
