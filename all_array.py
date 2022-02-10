1f7203fe260668378706b601e8f0c7422ba6d96e
yoloyoung <94785366+yoloyoung@users.noreply.github.com>
2022-02-10 22:56:31 +0200

    Update all_array.py

--- all_array.py
+++ all_array.py
@@ -1,4 +1,11 @@
 from main import *
+import sqlite3
+import asyncio
+import websockets
+import json
+import time
+from bs4 import BeautifulSoup
+import requests
 array = [
 'btc',
 'eth',
@@ -26,4 +33,4 @@ responsible = requests.get(url).text
 soup = BeautifulSoup(responsible, 'html.parser')
 nameOfCoin = soup.find_all("div", class_="css-1wp9rgv")
 for item in nameOfCoin:
-	coins = print(item.text.lower())
+	coins = (item.text.lower())
